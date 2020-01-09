934eebf201bb5f5545d2013d6376dbd36ff342e1
Harry Pooni <Harry.Pooni@amido.com>
2020-01-09 13:21:52 +0000

    Add project files.

---
+++ Data/CandidateProfile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorBaseProfileCascade.Data
+{
+    public class CandidateProfile : CandidateProfileBase
+    {
+        public CandidateProfile()
+        {
+            BaTechniques = new List<string>();
+            UxAndDesign = new List<string>();
+            Tools = new List<string>();
+            Certifications = new List<string>();
+        }
+
+        public IList<string> BaTechniques { get; set; }
+
+        public IList<string> UxAndDesign { get; set; }
+
+        public IList<string> Tools { get; set; }
+
+        public IList<string> Certifications { get; set; }
+    }
+}
---
+++ Data/CandidateProfileBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorBaseProfileCascade.Data
+{
+    public class CandidateProfileBase
+    {
+        public string EmailAddress { get; set; }
+
+        public string Summary { get; set; }
+
+        public IList<string> Strengths { get; set; } = new List<string>();
+
+        public IList<string> Expertise { get; set; } = new List<string>();
+    }
+}
